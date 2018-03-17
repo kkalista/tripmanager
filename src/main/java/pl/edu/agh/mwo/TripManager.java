d8f7be67859e6400c60a7eb715ba336a1c7f09da
student1 <student1@S7N-PROGLANG-53.pcoip.ki.agh.edu.pl>
2018-03-17 15:27:19 +0100

    test commit

--- src/main/java/pl/edu/agh/mwo/TripManager.java
+++ src/main/java/pl/edu/agh/mwo/TripManager.java
@@ -1,6 +1,9 @@
 package pl.edu.agh.mwo;
 
 public class TripManager {
-
+	
+	public int sum(int i, int j) {
+		return i + j;
+	}
 
 }
--- src/test/java/pl/edu/agh/mwo/TripManagerTest.java
+++ src/test/java/pl/edu/agh/mwo/TripManagerTest.java
@@ -1,6 +1,15 @@
 package pl.edu.agh.mwo;
- 
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class TripManagerTest {
 	
+	@Test
+	public void testSum() {
+		TripManager tm = new TripManager();
+		assertEquals(8, tm.sum(4, 4));
+	}
 
 }
