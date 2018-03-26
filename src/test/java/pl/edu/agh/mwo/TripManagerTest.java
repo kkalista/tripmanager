f80b7fd7ed62d5ff6e51538c0cad24ceaa9317ca
Katarzyna Kalista <katarzynakalista@wp.pl>
2018-03-26 20:55:55 +0200

    First implementation of unit tests for TripManager class

--- src/test/java/pl/edu/agh/mwo/TripManagerTest.java
+++ src/test/java/pl/edu/agh/mwo/TripManagerTest.java
@@ -1,7 +1,27 @@
 package pl.edu.agh.mwo;
 
+import org.junit.Test;
+
 public class TripManagerTest {
 	
+	@Test
+	public void testAddTrip() {
+		
+	}
 
+	@Test
+	public void testRemoveTrip() {
+		
+	}
+	
+	@Test
+	public void testGetTrips() {
+		
+	}
+	
+	@Test
+	public void testFindTrip() {
+		
+	}
 	}
 
