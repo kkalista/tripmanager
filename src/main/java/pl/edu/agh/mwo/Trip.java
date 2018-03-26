38ebd3b8492b7be238f5f5a3f82bff54655538f8
Katarzyna Kalista <katarzynakalista@wp.pl>
2018-03-26 20:36:36 +0200

    First implementation of Trip class

--- src/main/java/pl/edu/agh/mwo/Trip.java
+++ src/main/java/pl/edu/agh/mwo/Trip.java
@@ -5,8 +5,27 @@ import java.util.Collection;
 
 public class Trip {
 
+	private String name;
+	private String description;
+
 	Collection<Photo> photos = new ArrayList<Photo>();
-	
+
+	public String getName() {
+		return name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
 	public Collection<Photo> getPhotos() {
 		return photos;
 	}
