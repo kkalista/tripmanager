942c9993a6f407574f44bb11eeea0c69505af7a6
Katarzyna Kalista <katarzynakalista@wp.pl>
2018-03-31 13:42:25 +0200

    Photo class implementation

--- src/main/java/pl/edu/agh/mwo/Photo.java
+++ src/main/java/pl/edu/agh/mwo/Photo.java
@@ -1,7 +1,14 @@
 package pl.edu.agh.mwo;
 
-
 public class Photo {
 
+	private String comment;
 
+	public void setComment(String comment) {
+		this.comment = comment;
 	}
+
+	public String getComment() {
+		return comment;
+	}
+}
--- src/test/java/pl/edu/agh/mwo/PhotoTest.java
+++ src/test/java/pl/edu/agh/mwo/PhotoTest.java
@@ -1,11 +1,15 @@
 package pl.edu.agh.mwo;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class PhotoTest {
 
 	@Test
-	public void testComment() {
-		
+	public void testGetComment() {
+		Photo photo = new Photo();
+		photo.setComment("Amazing view!");
+		Assert.assertEquals("Amazing view!", photo.getComment());
 	}
+	
 }
